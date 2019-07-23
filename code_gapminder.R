6d2e363a7db90ed25749768bead4d735e5846a23
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-07-23 15:06:00 +1000

    complete lesson on ecological fallacy

--- code_gapminder.R
+++ code_gapminder.R
@@ -195,3 +195,24 @@ gapminder %>%
   scale_x_continuous(trans = "log2") +
   geom_density(alpha = 0.2, bw = 0.75, position = "stack") + facet_grid(year~.)
 
+gapminder <- gapminder %>%
+  mutate(group = case_when(
+    .$region %in% west ~ "The West",
+    .$region %in% "Northern Africa" ~ "Northern Africa",
+    .$region %in% c("Eastern Asia", "South-Eastern Asia") ~ "East Asia",
+    .$region == "Southern Asia" ~ "Southern Asia",
+    .$region %in% c("Central America", "South America", "Caribbean") ~ "Latin America",
+    .$continent == "Africa" & .$region != "Northern Africa" ~ "Sub-Saharan Africa",
+    .$region %in% c("Melanesia", "Micronesia", "Polynesia") ~ "Pacific Islands"))
+
+surv_income <- gapminder %>%
+  filter(year %in% present_year & !is.na(gdp) & !is.na(infant_mortality) & !is.na(group)) %>%
+  group_by(group) %>%
+  summarise(income = sum(gdp)/sum(population)/365,
+            infant_survival_rate = 1-sum(infant_mortality/1000*population)/sum(population))
+surv_income %>% arrange(income)
+
+surv_income %>% ggplot(aes(income, infant_survival_rate, label=group, color = group)) +
+  scale_x_continuous(trans = "log2", limit = c(0.25, 150)) +
+  scale_y_continuous(trans = "logit", limit = c(0.875, .9981), breaks = c(.85, .90,.95,.99,.995,.998)) +
+  geom_label(size = 3, show.legend = FALSE)
