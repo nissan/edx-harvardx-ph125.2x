33462a1612c607120371e8e3d59a28599d898813
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-07-23 14:48:26 +1000

    complete lesson on density plots

--- code_gapminder.R
+++ code_gapminder.R
@@ -153,3 +153,45 @@ p + geom_boxplot(aes(region, dollars_per_day, fill=continent)) +
 
 p + geom_boxplot(aes(region, dollars_per_day, fill=factor(year)))
 
+gapminder %>%
+  filter(year ==past_year & country %in% country_list) %>%
+  mutate(group = ifelse(region%in%west, "West", "Developing")) %>% group_by(group) %>%
+  summarise(n=n()) %>% knitr::kable()
+
+?n
+?geom_density
+
+p <- gapminder %>%
+  filter(year %in% c(past_year, present_year) & country %in% country_list) %>%
+  mutate(group = ifelse(region%in%west, "West", "Developing")) %>%
+  ggplot(aes(dollars_per_day, y = ..count.., fill = group)) +
+  scale_x_continuous(trans = "log2")
+p + geom_density(alpha = 0.2) + facet_grid(year~.)
+p + geom_density(alpha = 0.2, bw = 0.75) + facet_grid(year~.)
+
+gapminder <- gapminder %>% 
+  mutate(group = case_when(
+    .$region %in% west ~ "West",
+    .$region %in% c("Eastern Asia", "South-Eastern Asia") ~ "East Asia",
+    .$region %in% c("Caribbean", "Central America", "South America") ~ "Latin America",
+    .$continent == "Africa" & .$region != "Northern Africa" ~ "Sub-Saharan Africa",
+    TRUE ~ "Others"))
+
+gapminder <- gapminder %>%
+  mutate(group = factor(group, levels = c("Others", "Latin America", "East Asia", "Sub-Saharan Africa", "West")))
+p <- gapminder %>%
+  filter(year %in% c(past_year, present_year) & country %in% country_list) %>%
+  ggplot(aes(dollars_per_day, y = ..count.., fill = group)) +
+  scale_x_continuous(trans = "log2")
+p + geom_density(alpha = 0.2, bw = 0.75) + facet_grid(year~.)
+p + geom_density(alpha = 0.2, bw = 0.75, position = "stack") + facet_grid(year~.) 
+
+gapminder %>% 
+  filter(year %in% c(past_year, present_year) & country %in% country_list) %>%
+  group_by(year) %>%
+  mutate(weight = population/sum(population)*2) %>%
+  ungroup() %>%
+  ggplot(aes(dollars_per_day, fill = group, weight = weight)) +
+  scale_x_continuous(trans = "log2") +
+  geom_density(alpha = 0.2, bw = 0.75, position = "stack") + facet_grid(year~.)
+
