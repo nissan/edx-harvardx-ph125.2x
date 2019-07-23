d463e594466ca94c64c42fe6876802996f206619
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-07-23 12:07:08 +1000

    complete lesson on time-series

--- code_gapminder.R
+++ code_gapminder.R
@@ -1,4 +1,5 @@
 library(dslabs)
+library(tidyverse)
 data(gapminder)
 head(gapminder)
 
@@ -32,3 +33,27 @@ gapminder %>%
   ggplot(aes(fertility, life_expectancy, col=continent)) +
   geom_point() +
   facet_wrap(.~year)
+
+gapminder %>% filter(country == "United States") %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+countries <- c("South Korea", "Germany")
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility, group=country)) +
+  geom_line()
+
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility, col=country)) +
+  geom_line()
+
+labels <- data.frame(country = countries, x=c(1975, 1965), y=c(60,72))
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, life_expectancy, col = country)) + 
+  geom_line() +
+  geom_text(data = labels, aes(x, y, label = country), size = 5) + 
+  theme(legend.position = "none")
